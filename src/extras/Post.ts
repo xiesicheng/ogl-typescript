359bab3ace68b8faec3bdbe8f36d6aa707ab7123
nn <nshen121@gmail.com>
2019-11-01 10:48:04 +0800

    typescript

---
+++ src/extras/Post.ts
@@ -0,0 +1,178 @@
+// TODO: Destroy render targets if size changed and exists
+
+import { Geometry } from '../core/Geometry.js';
+import { Program } from '../core/Program.js';
+import { Mesh } from '../core/Mesh.js';
+import { RenderTarget } from '../core/RenderTarget.js';
+import { OGLRenderingContext } from '../core/Renderer.js';
+
+export interface PostOptions {
+    width: number;
+    height: number;
+    dpr: number;
+    wrapS: GLenum;  //gl.CLAMP_TO_EDGE,
+    wrapT: GLenum;  //gl.CLAMP_TO_EDGE,
+    minFilter: GLenum; // gl.LINEAR,
+    magFilter: GLenum; // gl.LINEAR,
+    geometry: Geometry;
+    targetOnly: any;
+}
+
+export interface Pass {
+
+    mesh: Mesh,
+    program: Program,
+    uniforms: any,
+    enabled: boolean,
+    textureUniform: any,
+
+}
+
+export class Post {
+
+    gl: OGLRenderingContext;
+    options: { wrapS: GLenum, wrapT: GLenum, minFilter: GLenum, magFilter: GLenum, width?: number, height?: number };
+    passes: Pass[];
+    geometry: Geometry;
+    uniform: { value: any };
+    targetOnly: any;
+    fbo: any;
+    dpr: number;
+    width: number;
+    height: number;
+
+    constructor(gl: OGLRenderingContext, {
+        width,
+        height,
+        dpr,
+        wrapS = gl.CLAMP_TO_EDGE,
+        wrapT = gl.CLAMP_TO_EDGE,
+        minFilter = gl.LINEAR,
+        magFilter = gl.LINEAR,
+        geometry = new Geometry(gl, {
+            position: { size: 2, data: new Float32Array([-1, -1, 3, -1, -1, 3]) },
+            uv: { size: 2, data: new Float32Array([0, 0, 2, 0, 0, 2]) },
+        }),
+        targetOnly = null,
+    }: Partial<PostOptions> = {}) {
+
+        this.gl = gl;
+
+        this.options = { wrapS, wrapT, minFilter, magFilter };
+
+        this.passes = [];
+
+        this.geometry = geometry;
+
+        this.uniform = { value: null };
+        this.targetOnly = targetOnly;
+
+        const fbo = this.fbo = {
+            read: null,
+            write: null,
+            swap: () => {
+                let temp = fbo.read;
+                fbo.read = fbo.write;
+                fbo.write = temp;
+            },
+        };
+
+        this.resize({ width, height, dpr });
+    }
+
+    addPass({
+        vertex = defaultVertex,
+        fragment = defaultFragment,
+        uniforms = {},
+        textureUniform = 'tMap',
+        enabled = true,
+    } = {}) {
+        uniforms[textureUniform] = { value: this.fbo.read.texture };
+
+        const program = new Program(this.gl, { vertex, fragment, uniforms });
+        const mesh = new Mesh(this.gl, { geometry: this.geometry, program });
+
+        const pass = {
+            mesh,
+            program,
+            uniforms,
+            enabled,
+            textureUniform,
+        };
+
+        this.passes.push(pass);
+        return pass;
+    }
+
+    resize({ width, height, dpr }: Partial<{ width: number, height: number, dpr: number }> = {}) {
+        if (dpr) this.dpr = dpr;
+        if (width) {
+            this.width = width;
+            this.height = height || width;
+        }
+
+        dpr = this.dpr || this.gl.renderer.dpr;
+        width = (this.width || this.gl.renderer.width) * dpr;
+        height = (this.height || this.gl.renderer.height) * dpr;
+
+        this.options.width = width;
+        this.options.height = height;
+
+        this.fbo.read = new RenderTarget(this.gl, this.options);
+        this.fbo.write = new RenderTarget(this.gl, this.options);
+    }
+
+    // Uses same arguments as renderer.render
+    render({
+        scene,
+        camera,
+        target = null,
+        update = true,
+        sort = true,
+        frustumCull = true,
+    }) {
+        const enabledPasses = this.passes.filter(pass => pass.enabled);
+
+        this.gl.renderer.render({
+            scene, camera,
+            target: enabledPasses.length || (!target && this.targetOnly) ? this.fbo.write : target,
+            update, sort, frustumCull,
+        });
+        this.fbo.swap();
+
+        enabledPasses.forEach((pass, i) => {
+            pass.mesh.program.uniforms[pass.textureUniform].value = this.fbo.read.texture;
+            this.gl.renderer.render({
+                scene: pass.mesh,
+                target: i === enabledPasses.length - 1 && (target || !this.targetOnly) ? target : this.fbo.write,
+                clear: i === enabledPasses.length - 1 ? true : false,
+            });
+            this.fbo.swap();
+        });
+
+        this.uniform.value = this.fbo.read.texture;
+    }
+}
+
+const defaultVertex = /* glsl */ `
+    attribute vec2 uv;
+    attribute vec2 position;
+
+    varying vec2 vUv;
+
+    void main() {
+        vUv = uv;
+        gl_Position = vec4(position, 0, 1);
+    }
+`;
+
+const defaultFragment = /* glsl */ `
+    precision highp float;
+
+    uniform sampler2D tMap;
+    varying vec2 vUv;
+
+    void main() {
+        gl_FragColor = texture2D(tMap, vUv);
+    }
+`;
---
+++ src/extras/Skin.ts
@@ -0,0 +1,141 @@
+import { Mesh } from '../core/Mesh.js';
+import { Transform } from '../core/Transform.js';
+import { Mat4 } from '../math/Mat4.js';
+import { Texture } from '../core/Texture.js';
+import { Animation } from './Animation.js';
+import { OGLRenderingContext } from '../core/Renderer.js';
+import { Geometry } from '../core/Geometry.js';
+import { Program } from '../core/Program.js';
+import { Camera } from '../core/Camera.js';
+
+const tempMat4 = new Mat4();
+
+export interface SkinOptions {
+    rig: any,
+    geometry: Geometry,
+    program: Program,
+    mode: GLenum //gl.TRIANGLES,
+}
+
+export interface BoneTransform extends Transform {
+    name: string;
+    bindInverse: Mat4;
+}
+
+export class Skin extends Mesh {
+
+    animations: Animation[];
+    boneTexture: Texture;
+    boneTextureSize: number;
+    boneMatrices: Float32Array;
+
+    root: Transform;
+    bones: BoneTransform[];
+
+    constructor(gl: OGLRenderingContext, {
+        rig,
+        geometry,
+        program,
+        mode = gl.TRIANGLES,
+    }: Partial<SkinOptions> = {}) {
+
+        super(gl, { geometry, program, mode });
+
+        this.createBones(rig);
+        this.createBoneTexture();
+        this.animations = [];
+
+        Object.assign(this.program.uniforms, {
+            boneTexture: { value: this.boneTexture },
+            boneTextureSize: { value: this.boneTextureSize },
+        });
+    }
+
+    createBones(rig) {
+
+        // Create root so that can simply update world matrix of whole skeleton
+        this.root = new Transform();
+
+        // Create bones
+        this.bones = [];
+        if (!rig.bones || !rig.bones.length) return;
+        for (let i = 0; i < rig.bones.length; i++) {
+            const bone = new Transform();
+
+            // Set initial values (bind pose)
+            bone.position.fromArray(rig.bindPose.position, i * 3)
+            bone.quaternion.fromArray(rig.bindPose.quaternion, i * 4);
+            bone.scale.fromArray(rig.bindPose.scale, i * 3);
+
+            this.bones.push(bone as BoneTransform);
+        };
+
+        // Once created, set the hierarchy
+        rig.bones.forEach((data: any, i: number) => {
+            this.bones[i].name = data.name;
+            if (data.parent === -1) return this.bones[i].setParent(this.root);
+            this.bones[i].setParent(this.bones[data.parent]);
+        });
+
+        // Then update to calculate world matrices
+        this.root.updateMatrixWorld(true);
+
+        // Store inverse of bind pose to calculate differences
+        this.bones.forEach(bone => {
+            bone.bindInverse = new Mat4(...bone.worldMatrix).inverse();
+        });
+    }
+
+    createBoneTexture() {
+        if (!this.bones.length) return;
+        const size = Math.max(4, Math.pow(2, Math.ceil(Math.log(Math.sqrt(this.bones.length * 4)) / Math.LN2)));
+        this.boneMatrices = new Float32Array(size * size * 4);
+        this.boneTextureSize = size;
+        this.boneTexture = new Texture(this.gl, {
+            image: this.boneMatrices,
+            generateMipmaps: false,
+            type: this.gl.FLOAT,
+            internalFormat: this.gl.renderer.isWebgl2 ? this.gl.RGBA16F : this.gl.RGBA,
+            flipY: false,
+            width: size,
+        });
+    }
+
+    addAnimation(data) {
+        const animation = new Animation({ objects: this.bones, data });
+        this.animations.push(animation);
+        return animation;
+    }
+
+    update() {
+
+        // Calculate combined animation weight
+        let total = 0;
+        this.animations.forEach(animation => total += animation.weight);
+
+        this.animations.forEach((animation, i) => {
+
+            // force first animation to set in order to reset frame
+            animation.update(total, i === 0);
+        });
+    }
+
+    draw({
+        camera,
+    }: { camera?: Camera } = {}) {
+
+        // Update world matrices manually, as not part of scene graph
+        this.root.updateMatrixWorld(true);
+
+        // Update bone texture
+        this.bones.forEach((bone, i) => {
+
+            // Find difference between current and bind pose
+            tempMat4.multiply(bone.worldMatrix, bone.bindInverse);
+            this.boneMatrices.set(tempMat4, i * 16);
+        });
+        if (this.boneTexture) this.boneTexture.needsUpdate = true;
+
+        super.draw({ camera });
+    }
+}

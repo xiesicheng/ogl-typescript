4c37df9f935592d035c09a8012fffafb090f125c
nshen <nshen121@gmail.com>
2020-02-14 17:48:42 +0800

    add comment

--- src/extras/Triangle.ts
+++ src/extras/Triangle.ts
@@ -1,14 +1,21 @@
 import { Geometry } from '../core/Geometry';
 
 export class Triangle extends Geometry {
-    constructor(gl, { 
+    constructor(gl, {
         attributes = {},
     } = {}) {
+
+        //         position                uv
+        //      (-1, 3)                  (0, 2)
+        //         |\                      |\
+        //         |__\(1, 1)              |__\(1, 1)
+        //         |__|_\                  |__|_\
+        //   (-1, -1)   (3, -1)        (0, 0)   (2, 0)
 
         Object.assign(attributes, {
             position: { size: 2, data: new Float32Array([-1, -1, 3, -1, -1, 3]) },
             uv: { size: 2, data: new Float32Array([0, 0, 2, 0, 0, 2]) },
-        }); 
+        });
 
         super(gl, attributes);
     }
